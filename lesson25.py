4ec9e21efc789548145ab6aa1f8fb906b4d5c5f4
Javohir_Qurbonov <javohirqurbonov0109@gmail.com>
2026-02-05 19:18:44 +0500

    My codes in learning Python

---
+++ lesson25.py
@@ -0,0 +1,17 @@
+# class Grandparent:
+#     def method1(self):
+#         print("Bu bobomizning metodi")
+#
+# class Parent(Grandparent):
+#     def method2(self):
+#         print("Bu otamizning metodi")
+#
+# class Child(Parent):
+#     def method3(self):
+#         print("Bu farzandning metodi")
+#
+# obj = Child()
+# obj.method1()  # Bu bobomizning metodi
+# obj.method2()  # Bu otamizning metodi
+# obj.method3()  # Bu farzandning metodi
+
